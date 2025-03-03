27fca62a3a95f2b0b6c9ba5995035b87aa16a9e5
Samyak Sukhdeve <samyaksukhadeve12@gmail.com>
2025-03-03 17:43:11 +0530

    minor fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import Protected from "./Protected";
 import DashBoard from "./pages/DashBoard";
 
