9320d7b69a7a56e8229d8271b369943ff9e4baba
Samyak Sukhdeve <samyaksukhadeve12@gmail.com>
2025-03-03 18:53:18 +0530

    updated svg imports

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/useAuthStore";
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
-
+import google from "../assets/google.svg";
+import github from "../assets/github.svg";
 const Signup = () => {
   const { signUpWithGoogle, user, signUpWithGitHub } = useAuthStore();
   const navigate = useNavigate();
@@ -18,11 +19,11 @@ const Signup = () => {
       <span className="py-10 text-2xl">SignUp or LogIn</span>
       <div className="grid md:grid-cols-2 gap-6">
         <div className="flex flex-col justify-center bg-amber-50 p-5 rounded gap-5">
-          <img src="/src/assets/google.svg" className="h-8" alt="" />
+          <img src={google} className="h-8" alt="" />
           <Button onClick={signUpWithGoogle}>Sign up with Google</Button>
         </div>
         <div className="flex flex-col justify-center bg-amber-50 p-5 rounded gap-5">
-          <img src="/src/assets/github.svg" className="h-8" alt="" />
+          <img src={github} className="h-8" alt="" />
           <Button onClick={signUpWithGitHub}>Sign up with Github</Button>
         </div>
       </div>
